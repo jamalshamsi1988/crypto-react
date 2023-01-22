ac8d48f7eda90952c61390cd6ec3c2865a53df49
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-01-22 04:51:15 -0800

    import api and create landing page

---
+++ src/components/Landing.js
@@ -0,0 +1,36 @@
+import React,{useEffect , useState} from 'react';
+//API
+import { getCoin } from '../services/api';
+//components
+import Loader from './Loader';
+
+
+const Landing = () => {
+    
+    const [coins , setCoins]= useState([]);
+
+    useEffect(()=>{
+        const fetchAPI=async()=>{
+              const data=  await getCoin();
+         
+                  setCoins(data);
+        }
+        fetchAPI();
+
+    },[])
+
+  return (
+    <>
+      <input type="text" placeholder='Search'/>
+      {
+        coins.length ?
+         <div>
+                {coins.map(coin => <p key={coin.id} >{coin.name}</p>)}
+        </div>
+        : <Loader />
+      }
+    </>
+  )
+}
+
+export default Landing
